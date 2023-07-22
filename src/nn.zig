ba42382485ecec9030ee58f55243b1f1725b9760
Martin Smits <msmits@ommos.net>
2023-07-22 15:43:27 +0200

    Implemented more generalized neural network functions

--- src/nn.zig
+++ src/nn.zig
@@ -1,8 +1,13 @@
 const std = @import("std");
+const print = std.debug.print;
 
 // Setup random number generator
 const RndGen = std.rand.DefaultPrng;
 var rnd: std.rand.DefaultPrng = undefined;
+
+// General purpose allocator
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 
 pub const Mat = struct {
     rows: usize,
@@ -32,11 +37,11 @@ pub fn mat_alloc(rows: usize, cols: usize) !Mat {
     return mat;
 }
 
-pub fn mat_get(mat: Mat, i: usize, j: usize) @TypeOf(mat.es[0]) {
+pub fn mat_get(mat: Mat, i: usize, j: usize) f32 {
     return mat.es[i * mat.stride + j];
 }
 
-pub fn mat_put(mat: Mat, i: usize, j: usize, v: @TypeOf(mat.es[0])) void {
+pub fn mat_put(mat: Mat, i: usize, j: usize, v: f32) void {
     mat.es[i * mat.stride + j] = v;
 }
 
@@ -63,8 +68,8 @@ pub fn mat_rand(dst: Mat, low: f32, high: f32) void {
 }
 
 pub fn mat_row(mat: Mat, index: usize) Mat {
-    const offset: usize = index * mat.cols;
-    var row = Mat{ .rows = 1, .cols = mat.cols, .stride = mat.cols, .es = mat.es[offset .. offset + mat.cols] };
+    const offset: usize = index * mat.stride;
+    var row = Mat{ .rows = 1, .cols = mat.cols, .stride = mat.stride, .es = mat.es[offset .. offset + mat.cols] };
 
     return row;
 }
@@ -124,7 +129,7 @@ pub fn mat_sigmoid(mat: Mat) void {
 pub fn mat_print(mat: Mat, name: [*:0]const u8) void {
     std.debug.assert(mat.rows != 0);
     std.debug.assert(mat.cols != 0);
-    std.debug.print("{s} = [\n", .{name});
+    std.debug.print("{s} [{d}x{d}] = [\n", .{ name, mat.rows, mat.cols });
     var i: usize = 0;
     while (i < mat.rows) : (i += 1) {
         var j: usize = 0;
@@ -135,4 +140,156 @@ pub fn mat_print(mat: Mat, name: [*:0]const u8) void {
         std.debug.print("\n", .{});
     }
     std.debug.print("]\n", .{});
+}
+
+pub const NN = struct {
+    count: usize,
+    ws: []Mat,
+    bs: []Mat,
+    as: []Mat, // The amount of activations is count+1
+};
+
+pub fn nn_alloc(arch: [*]u32, arch_count: usize) !NN {
+    std.debug.assert(arch_count > 0);
+    var model = NN{ .count = arch_count - 1, .ws = undefined, .bs = undefined, .as = undefined };
+    model.as = try allocator.alloc(Mat, arch_count);
+    model.ws = try allocator.alloc(Mat, arch_count - 1);
+    model.bs = try allocator.alloc(Mat, arch_count - 1);
+    model.as[0] = try mat_alloc(1, arch[0]);
+    var i: usize = 1;
+    while (i < arch_count) : (i += 1) {
+        model.ws[i - 1] = try mat_alloc(model.as[i - 1].cols, arch[i]);
+        model.bs[i - 1] = try mat_alloc(1, arch[i]);
+        model.as[i] = try mat_alloc(1, arch[i]);
+    }
+
+    return model;
+}
+
+pub fn nn_input(model: NN) Mat {
+    return model.as[0];
+}
+
+pub fn nn_output(model: NN) Mat {
+    return model.as[model.count];
+}
+
+pub fn nn_rand(model: NN, low: f32, high: f32) void {
+    var i: usize = 0;
+    while (i < model.count) : (i += 1) {
+        mat_rand(model.ws[i], low, high);
+        mat_rand(model.bs[i], low, high);
+    }
+}
+pub fn nn_forward(model: NN) void {
+    var i: usize = 0;
+    while (i < model.count) : (i += 1) {
+        mat_dot(model.as[i + 1], model.as[i], model.ws[i]);
+        mat_sum(model.as[i + 1], model.bs[i]);
+        mat_sigmoid(model.as[i + 1]);
+    }
+    //nn_print(model, "forward");
+}
+
+pub fn nn_cost(model: NN, ti: Mat, to: Mat) f32 {
+    std.debug.assert(ti.rows == to.rows);
+    std.debug.assert(to.cols == nn_output(model).cols);
+
+    var cost: f32 = 0;
+    var i: usize = 0;
+    while (i < ti.rows) : (i += 1) {
+        var x = nn_input(model);
+        var y = nn_output(model);
+
+        mat_copy(x, mat_row(ti, i));
+        var expected = mat_row(to, i);
+        nn_forward(model);
+        var j: usize = 0;
+        while (j < to.cols) : (j += 1) {
+            var d: f32 = mat_get(y, 0, j) - mat_get(expected, 0, j);
+            cost += d * d;
+        }
+    }
+    return cost / @intToFloat(f32, ti.rows);
+}
+
+pub fn nn_finite_diff(model: NN, gradient: NN, eps: f32, ti: Mat, to: Mat) void {
+    var saved: f32 = undefined;
+    var cost: f32 = nn_cost(model, ti, to);
+
+    var k: usize = 0;
+    while (k < model.count) : (k += 1) {
+        var i: usize = 0;
+        while (i < model.ws[k].rows) : (i += 1) {
+            var j: usize = 0;
+            while (j < model.ws[k].cols) : (j += 1) {
+                saved = mat_get(model.ws[k], i, j);
+                mat_put(model.ws[k], i, j, saved + eps);
+                mat_put(gradient.ws[k], i, j, (nn_cost(model, ti, to) - cost) / eps);
+                mat_put(model.ws[k], i, j, saved);
+            }
+        }
+        i = 0;
+        while (i < model.bs[k].rows) : (i += 1) {
+            var j: usize = 0;
+            while (j < model.bs[k].cols) : (j += 1) {
+                saved = mat_get(model.bs[k], i, j);
+                mat_put(model.bs[k], i, j, saved + eps);
+                mat_put(gradient.bs[k], i, j, (nn_cost(model, ti, to) - cost) / eps);
+                mat_put(model.bs[k], i, j, saved);
+            }
+        }
+    }
+}
+
+pub fn nn_learn(model: NN, gradient: NN, rate: f32) void {
+    var k: usize = 0;
+    while (k < model.count) : (k += 1) {
+        var i: usize = 0;
+        while (i < model.ws[k].rows) : (i += 1) {
+            var j: usize = 0;
+            while (j < model.ws[k].cols) : (j += 1) {
+                mat_put(model.ws[k], i, j, mat_get(model.ws[k], i, j) - rate * mat_get(gradient.ws[k], i, j));
+            }
+        }
+        i = 0;
+        while (i < model.bs[k].rows) : (i += 1) {
+            var j: usize = 0;
+            while (j < model.bs[k].cols) : (j += 1) {
+                mat_put(model.bs[k], i, j, mat_get(model.bs[k], i, j) - rate * mat_get(gradient.bs[k], i, j));
+            }
+        }
+    }
+}
+
+pub fn nn_print(model: NN, name: [*:0]const u8) void {
+    print("{s} = [\n", .{name});
+    print("  as[{d:3}]: {d} x {d}, [", .{ 0, model.as[0].rows, model.as[0].cols });
+    var j: usize = 0;
+    while (j < model.as[0].rows * model.as[0].cols) : (j += 1) {
+        print("{d:.6} ", .{model.as[0].es[j]});
+    }
+    print("],\n", .{});
+    var i: usize = 0;
+    while (i < model.count) : (i += 1) {
+        print("  ws[{d:3}]: {d} x {d}, [", .{ i, model.ws[i].rows, model.ws[i].cols });
+        j = 0;
+        while (j < model.ws[i].rows * model.ws[i].cols) : (j += 1) {
+            print("{d:.6} ", .{model.ws[i].es[j]});
+        }
+        print("],\n", .{});
+        print("  bs[{d:3}]: {d} x {d}, [", .{ i, model.bs[i].rows, model.bs[i].cols });
+        j = 0;
+        while (j < model.bs[i].rows * model.bs[i].cols) : (j += 1) {
+            print("{d:.6} ", .{model.bs[i].es[j]});
+        }
+        print("],\n", .{});
+        print("  as[{d:3}]: {d} x {d}, [", .{ i + 1, model.as[i + 1].rows, model.as[i + 1].cols });
+        j = 0;
+        while (j < model.as[i + 1].rows * model.as[i + 1].cols) : (j += 1) {
+            print("{d:.6} ", .{model.as[i + 1].es[j]});
+        }
+        print("],\n", .{});
+    }
+    print("]\n", .{});
 }
--- src/nn_main.zig
+++ src/nn_main.zig
@@ -1,209 +1,74 @@
 const std = @import("std");
+const print = std.debug.print;
 
 const nn = @import("nn.zig");
 const Mat = nn.Mat;
-const mat_alloc = nn.mat_alloc;
-const mat_dot = nn.mat_dot;
-const mat_sum = nn.mat_sum;
-const mat_get = nn.mat_get;
 const mat_put = nn.mat_put;
-const mat_sigmoid = nn.mat_sigmoid;
-const mat_row = nn.mat_row;
-const mat_copy = nn.mat_copy;
-const mat_rand = nn.mat_rand;
-const mat_print = nn.mat_print;
+const mat_get = nn.mat_get;
+const NN = nn.NN;
+const nn_init = nn.nn_init;
+const nn_alloc = nn.nn_alloc;
+const nn_rand = nn.nn_rand;
+const nn_cost = nn.nn_cost;
+const nn_finite_diff = nn.nn_finite_diff;
+const nn_learn = nn.nn_learn;
+const nn_forward = nn.nn_forward;
+const nn_input = nn.nn_input;
+const nn_output = nn.nn_output;
 
-var xor_train = [_]f32{
+// training data
+var train_xor = [_]f32{
     0, 0, 0,
     0, 1, 1,
     1, 0, 1,
     1, 1, 0,
 };
-const train_xor_mat = Mat{ .rows = 4, .cols = 3, .stride = 3, .es = &xor_train };
-
-var train_mat_in = Mat{ .rows = 4, .cols = 2, .stride = 3, .es = train_xor_mat.es };
-var train_mat_out = Mat{ .rows = 4, .cols = 1, .stride = 3, .es = xor_train[2..] };
-
-const Xor = struct {
-    a0: Mat,
-    w1: Mat,
-    b1: Mat,
-    a1: Mat,
-    w2: Mat,
-    b2: Mat,
-    a2: Mat,
+var train_or = [_]f32{
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 1,
 };
 
-fn xor_alloc() !Xor {
-    var model: Xor = undefined;
-    model.a0 = try mat_alloc(1, 2);
-    // layer 1
-    model.w1 = try mat_alloc(2, 2);
-    model.b1 = try mat_alloc(1, 2);
-    model.a1 = try mat_alloc(1, 2);
-    // layer 2
-    model.w2 = try mat_alloc(2, 1);
-    model.b2 = try mat_alloc(1, 1);
-    model.a2 = try mat_alloc(1, 1);
-
-    return model;
-}
-
-fn forward(model: Xor) void {
-    // a1 = sigmoid(x * w1 + b)
-    mat_dot(model.a1, model.a0, model.w1);
-    mat_sum(model.a1, model.b1);
-    mat_sigmoid(model.a1);
-
-    // a2 = sigmoid(a1 * w2 + b2)
-    mat_dot(model.a2, model.a1, model.w2);
-    mat_sum(model.a2, model.b2);
-    mat_sigmoid(model.a2);
-}
-
-fn cost(model: Xor, ti: Mat, to: Mat) f32 {
-    std.debug.assert(ti.rows == to.rows);
-    std.debug.assert(to.cols == model.a2.cols);
-
-    var c: f32 = 0;
-    var i: usize = 0;
-    while (i < ti.rows) : (i += 1) {
-        var x = mat_row(ti, i);
-        var y = mat_row(to, i);
-
-        mat_copy(model.a0, x);
-        //mat_print(x, "x");
-        //mat_print(model.a0, "a0");
-        forward(model);
-
-        var j: usize = 0;
-        while (j < to.cols) : (j += 1) {
-            var diff = mat_get(model.a2, 0, j) - mat_get(y, 0, j);
-            c += diff * diff;
-        }
-    }
-    return c / @intToFloat(f32, ti.rows);
-}
-
-fn finite_diff(model: Xor, gradient: Xor, eps: f32, ti: Mat, to: Mat) void {
-    var c = cost(model, ti, to);
-    var saved: f32 = undefined;
-
-    var i: usize = 0;
-    while (i < model.w1.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.w1.cols) : (j += 1) {
-            saved = mat_get(model.w1, i, j);
-            mat_put(model.w1, i, j, saved + eps);
-            mat_put(gradient.w1, i, j, (cost(model, ti, to) - c) / eps);
-            mat_put(model.w1, i, j, saved);
-        }
-    }
-    i = 0;
-    while (i < model.b1.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.b1.cols) : (j += 1) {
-            saved = mat_get(model.b1, i, j);
-            mat_put(model.b1, i, j, saved + eps);
-            mat_put(gradient.b1, i, j, (cost(model, ti, to) - c) / eps);
-            mat_put(model.b1, i, j, saved);
-        }
-    }
-    i = 0;
-    while (i < model.w2.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.w2.cols) : (j += 1) {
-            saved = mat_get(model.w2, i, j);
-            mat_put(model.w2, i, j, saved + eps);
-            mat_put(gradient.w2, i, j, (cost(model, ti, to) - c) / eps);
-            mat_put(model.w2, i, j, saved);
-        }
-    }
-    i = 0;
-    while (i < model.b2.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.b2.cols) : (j += 1) {
-            saved = mat_get(model.b2, i, j);
-            mat_put(model.b2, i, j, saved + eps);
-            mat_put(gradient.b2, i, j, (cost(model, ti, to) - c) / eps);
-            mat_put(model.b2, i, j, saved);
-        }
-    }
-}
-
-fn xor_learn(model: Xor, gradient: Xor, rate: f32) void {
-    var i: usize = 0;
-    while (i < model.w1.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.w1.cols) : (j += 1) {
-            var value = mat_get(model.w1, i, j) - rate * mat_get(gradient.w1, i, j);
-            mat_put(model.w1, i, j, value);
-        }
-    }
-    i = 0;
-    while (i < model.b1.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.b1.cols) : (j += 1) {
-            var value = mat_get(model.b1, i, j) - rate * mat_get(gradient.b1, i, j);
-            mat_put(model.b1, i, j, value);
-        }
-    }
-    i = 0;
-    while (i < model.w2.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.w2.cols) : (j += 1) {
-            var value = mat_get(model.w2, i, j) - rate * mat_get(gradient.w2, i, j);
-            mat_put(model.w2, i, j, value);
-        }
-    }
-    i = 0;
-    while (i < model.b2.rows) : (i += 1) {
-        var j: usize = 0;
-        while (j < model.b2.cols) : (j += 1) {
-            var value = mat_get(model.b2, i, j) - rate * mat_get(gradient.b2, i, j);
-            mat_put(model.b2, i, j, value);
-        }
-    }
-}
-
 pub fn main() !void {
-    var model: Xor = try xor_alloc();
-    var gradient: Xor = try xor_alloc();
+    nn_init();
 
-    mat_rand(model.w1, 0, 1);
-    mat_rand(model.b1, 0, 1);
-    mat_rand(model.w2, 0, 1);
-    mat_rand(model.b2, 0, 1);
+    const train_mat = Mat{ .rows = 4, .cols = 3, .stride = 3, .es = &train_xor };
+    const ti = Mat{ .rows = 4, .cols = 2, .stride = 3, .es = train_mat.es };
+    const to = Mat{ .rows = 4, .cols = 1, .stride = 3, .es = train_mat.es[2..] };
+
+    // setup the network architecture
+    var arch = [_]u32{ 2, 2, 1 };
+    var model: NN = try nn_alloc(&arch, arch.len);
+    var gradient: NN = try nn_alloc(&arch, arch.len);
+    nn_rand(model, 0, 1);
 
     const eps: f32 = 1e-1;
     const rate: f32 = 1e-1;
 
-    std.debug.print("----- training data ------\n", .{});
-    mat_print(train_mat_in, "ti");
-    mat_print(train_mat_out, "t0");
-
     std.debug.print("--------------------------\n", .{});
-    std.debug.print("cost = {d:.6}\n", .{cost(model, train_mat_in, train_mat_out)});
-    var n: usize = 0;
-    const iter_count: usize = 10 * 1000;
-    while (n < iter_count) : (n += 1) {
-        finite_diff(model, gradient, eps, train_mat_in, train_mat_out);
-        xor_learn(model, gradient, rate);
-
-        std.debug.print("{d:6}: cost = {d:.6}\n", .{ n, cost(model, train_mat_in, train_mat_out) });
-    }
-
-    std.debug.print("--------------------------\n", .{});
+    //   nn_print(model, "xor_model");
+    const iter_count: usize = 100 * 1000;
+    std.debug.print("Running {d} iterations...\n", .{iter_count});
+    std.debug.print("Cost = {d:.6}\n", .{nn_cost(model, ti, to)});
     var i: usize = 0;
+    while (i < iter_count) : (i += 1) {
+        nn_finite_diff(model, gradient, eps, ti, to);
+        nn_learn(model, gradient, rate);
+        //        nn_print(model, "xor_model");
+    }
+    //    nn_print(model, "xor_model");
+    std.debug.print("Cost = {d:.6}\n", .{nn_cost(model, ti, to)});
+
+    std.debug.print("--------------------------\n", .{});
+    i = 0;
     while (i < 2) : (i += 1) {
         var j: usize = 0;
         while (j < 2) : (j += 1) {
-            mat_put(model.a0, 0, 0, @intToFloat(f32, i));
-            mat_put(model.a0, 0, 1, @intToFloat(f32, j));
-            forward(model);
-            var y: f32 = mat_get(model.a2, 0, 0);
-
-            std.debug.print("{d} ^ {d} = {d:.6}\n", .{ i, j, y });
+            mat_put(nn_input(model), 0, 0, @intToFloat(f32, i));
+            mat_put(nn_input(model), 0, 1, @intToFloat(f32, j));
+            nn_forward(model);
+            std.debug.print("{d} ^ {d} = {d:.6}\n", .{ i, j, mat_get(nn_output(model), 0, 0) });
         }
     }
 }
